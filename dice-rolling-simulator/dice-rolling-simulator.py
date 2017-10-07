e0261b48809c12c48a1f6ab65c040afd4fc3d667
Michael Persson <michael.persson.se@gmail.com>
2017-10-07 17:27:08 +0200

    Adding the basic game flow

--- dice-rolling-simulator/dice-rolling-simulator.py
+++ dice-rolling-simulator/dice-rolling-simulator.py
@@ -2,6 +2,14 @@ import random
 
 print('Welcome to the Dice Rolling Simulator!')
 
-def roll():
-    i = random.randrange(1, 7)
-    print('You rolled ' + str(i))
+nbrOfDices = input('How many dices do you want to throw? \n')
+
+
+def roll(nbr_of_dices):
+    while nbr_of_dices > 0:
+        i = random.randrange(1, 7)
+        print('You rolled ' + str(i))
+        nbr_of_dices -= 1
+
+
+roll(int(nbrOfDices))
