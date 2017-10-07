7b0489ceaf7fb136e29cb8f1e3bd84a94b11a72c
Michael Persson <michael.persson.se@gmail.com>
2017-10-07 20:45:33 +0200

    First commit for a simple black jack game.

--- black-jack/black-jack.py
+++ black-jack/black-jack.py
@@ -1,1 +1,19 @@
+import random
+
 print('Welcome to Black Jack! The coolest casino game ever!')
+
+card_sum = 0
+
+print('Your sum is {}'.format(card_sum))
+print('Do you want to draw another card?')
+
+
+
+
+def draw_card():
+    cardValue = random.randint(1, 11)
+    return cardValue
+
+card_sum = draw_card()
+
+print(card_sum)
