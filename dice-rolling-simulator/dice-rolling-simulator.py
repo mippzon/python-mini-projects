7821855fc079f273fa3d61a4d7ae158d2f77ca8a
Michael Persson <michael.persson.se@gmail.com>
2017-10-07 17:31:15 +0200

    Added a way to show the sum of the dices thrown

--- dice-rolling-simulator/dice-rolling-simulator.py
+++ dice-rolling-simulator/dice-rolling-simulator.py
@@ -6,10 +6,15 @@ nbrOfDices = input('How many dices do you want to throw? \n')
 
 
 def roll(nbr_of_dices):
+
+    sum = 0
+
     while nbr_of_dices > 0:
         i = random.randrange(1, 7)
+        sum += i
         print('You rolled ' + str(i))
         nbr_of_dices -= 1
 
+    print('You rolled a total of: ' + str(sum))
 
 roll(int(nbrOfDices))
