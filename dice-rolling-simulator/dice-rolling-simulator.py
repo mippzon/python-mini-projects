cbb09de73a25f5a7b8923f2b1c2ef311e503cb00
Michael Persson <michael.persson.se@gmail.com>
2017-10-07 17:40:38 +0200

    Adding the more pythonic way of displaying ints in strings

--- dice-rolling-simulator/dice-rolling-simulator.py
+++ dice-rolling-simulator/dice-rolling-simulator.py
@@ -12,9 +12,9 @@ def roll(nbr_of_dices):
     while nbr_of_dices > 0:
         i = random.randrange(1, 7)
         sum += i
-        print('You rolled ' + str(i))
+        print('You rolled {}'.format(i))
         nbr_of_dices -= 1
 
-    print('You rolled a total of: ' + str(sum))
+    print('You rolled a total of: {}'.format(sum))
 
 roll(int(nbrOfDices))
