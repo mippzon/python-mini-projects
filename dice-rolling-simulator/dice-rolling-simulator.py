cebed75648a69586b563bd11221274ed4126c95e
Michael Persson <michael.persson.se@gmail.com>
2017-10-07 17:17:12 +0200

    Adding a roll function

--- dice-rolling-simulator/dice-rolling-simulator.py
+++ dice-rolling-simulator/dice-rolling-simulator.py
@@ -2,5 +2,6 @@ import random
 
 print('Welcome to the Dice Rolling Simulator!')
 
-i = random.randrange(1, 7)
-print('You rolled ' + str(i))
+def roll():
+    i = random.randrange(1, 7)
+    print('You rolled ' + str(i))
