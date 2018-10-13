144b4baf35b6e4876da009002da16ce63160b38a
Michael Persson <michael.persson.se@gmail.com>
2018-10-13 14:23:19 +0200

    First verstion of rock-paper-scissors.py

---
+++ practice-python/08-rock-paper-scissor.py
@@ -0,0 +1,9 @@
+print('Welcome to the Rock Paper Scissor game!')
+
+player_points = 0
+computer_points = 0
+
+possible_actions = ['rock', 'paper', 'scissor']
+
+player_action = int(input('Enter 1 for Rock, 2 for Paper or 3 for Scissor: '))
+
