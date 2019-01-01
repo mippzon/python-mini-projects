3f4d22ea63fa297f10a330a1da68bbc773d535f8
Michael Persson <michael.persson.se@gmail.com>
2019-01-01 17:18:01 +0100

    Add first version of interactive dictionary

---
+++ interactive-dict/dict.py
@@ -0,0 +1,34 @@
+import json
+import difflib
+from difflib import get_close_matches
+
+data = json.load(open('D:\dev\python-mini-projects\interactive-dict\dictionary.json'))
+
+def retrive_definition(word):
+
+    word = word.lower()
+
+    if word in data:
+        return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        action = input('Did you mean %s instead? [y or n]' % get_close_matches(word, data.keys())[0])
+        if action == 'y':
+            return data[get_close_matches(word, data.keys())[0]]
+        elif action == 'n':
+            return 'Word does not exist'
+        else:
+            return 'We dont understand'
+
+word_user = input('Enter a word: ')
+
+output = retrive_definition(word_user)
+
+if type(output) == list:
+    for item in output:
+        print('-', item)
+else:
+    print('-', output)
